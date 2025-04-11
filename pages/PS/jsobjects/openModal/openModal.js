50963ec005373485e3403581c6afff0679e91753
Agus Supriadi <agus.supriadi@evermos.com>
2025-04-11 08:53:35 +0000

    Update test

--- pages/PS/jsobjects/openModal/openModal.js
+++
@@ -1,8 +0,0 @@
-export default {
-	handleRowModal: () => {
-		const rowId = appsmith.URL.queryParams.rowId;
-		if (rowId) {
-			storeValue("targetRowId", rowId);  // simpan rowId ke local store
-		}
-	}
-}
