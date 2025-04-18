dc1381a778fc9d7978c4058ad569b970d5a431f2
agus.supriadi <agus.supriadi@evermos.com>
2025-04-18 11:16:03 +0000

    Testing on page load

---
+++ pages/INFO GENERAL/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,8 @@
+export default {
+  checkRowId() {
+    if (Find_GENERAL.data.length === 0) {
+      showAlert("Halaman tidak ditemukan", "error");
+      navigateTo("/not-found-page", {}, "SAME_WINDOW");
+    }
+  }
+}
